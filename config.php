a1742f7dd45634f1b21680d788952931afa1284f
Vladimir Migachev <debobaher@gmail.com>
2020-05-13 18:47:03 +0300

    New file and some initialisation

--- config.php
+++ config.php
@@ -1,1 +1,18 @@
-<?php  ?>
+<?php
+/*
+ * dbbhInstaller sample configuration file.
+ * The parameters may have default values. If so they will be typed in the input elements.
+ * The comment (//) after the parameter is the name for the parameneter at the installation script.
+ * The comment (//) without parameter is the section header.
+ * This is a sample configuration file. These first parameters must exist to connect to the database.
+ *
+ * To run the installation script - execute install.php
+*/
+
+// Database connection parameters
+$db_host = '';      // Database server address
+$db_user = '';      // Database server username
+$db_password = '';  // Database server password
+$db_database = '';  // Database name
+$db_prefix = '';    // Database tables prefix
+?>
---
+++ install.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * dbbhInstaller installation script.
+ * Please prepare config.php and db.sql files to perform the installation.
+ * Installation act with two phases:
+ * 1. The config.php parameters configuration.
+ * 2. Executing the db.sql script to fill the initial structure of the database.
+*/
+
+
+ ?>
